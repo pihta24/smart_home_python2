33f39d0e1e709f59ecd6697fab8e882ba502c153
Ivan <kruglovivan005@gmail.com>
2021-03-19 23:34:03 +0300

    url prefix delete

--- wsgi.py
+++ wsgi.py
@@ -1,15 +1,4 @@
-from werkzeug.middleware.dispatcher import DispatcherMiddleware
 from app import app
-
-
-def simple(env, resp):
-    resp(b'200 OK', [('Content-Type', 'text/plain')])
-    return [b'Hello WSGI World']
-
-
-app.config["APPLICATION_ROOT"] = "/ivan"
-app.wsgi_app = DispatcherMiddleware(simple, {'/ivan': app.wsgi_app})
-
 
 if __name__ == '__main__':
     app.run()
